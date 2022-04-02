c59b59bfa326d0b564fdb7238cc907a8b612b637
Beloslava <beloslava.malakova@gmail.com>
2022-04-02 14:38:34 +0300

    API

--- main.js
+++ main.js
@@ -1,35 +1,24 @@
-function printSquareMatrix(size) {
-    size <= 20;
-    var mat = Array.from(Array(size), () => new Array(size));
-    for (var i = 0; i <= size - 1; i++) {
-        for (var j = 0; j < size - 1; j++) {
-          mat[i][j] = '*';
-        }
-    mat[i][j] = '*';
-    }
-  return mat;
+const nums = ['10', '17', '5', '42', '44', '31']
+br = 0
+var sum = 0
+for (var i = 0; i < elmt.length; i++) {
+  sum += parseInt(nums[i], 10)
+}
+var avg = sum / nums.length
+document.write(
+  'The sum of all the elements is: ' + sum + ' The average is: ' + avg
+)
+
+function Minmaxnum () {
+  var max_of_array = Math.max.apply(Math, nums)
+  var min_of_array = Math.min.apply(Math, nums)
+
+  return max_of_array, min_of_array
 }
 
-    const nums = ['10', '17', '5', '42', '44', '31'];
-    br=0;
-    var sum = 0;
-    for( var i = 0; i < elmt.length; i++ ){
-        sum += parseInt( nums[i], 10 ); 
-    }
-    var avg = sum/nums.length;
-    document.write( "The sum of all the elements is: " + sum + " The average is: " + avg );
-
-    function Minmaxnum(){
-        var max_of_array = Math.max.apply(Math, nums);
-        var min_of_array = Math.min.apply(Math, nums);
-
-            return max_of_array, min_of_array;
-    }
-
-
-    x; //asign value for x
-    let txt='';
-    for (i=0; i<x; i++){
-        txt += x[i] + ' ';
-    }
-    document.getElementById("till1").innerHTML = txt;
+x //asign value for x
+let txt = ''
+for (i = 0; i < x; i++) {
+  txt += x[i] + ' '
+}
+document.getElementById('till1').innerHTML = txt
--- user_list.js
+++ user_list.js
@@ -1,10 +1,23 @@
-let xml = new XMLHttpRequest()
-xml.open(
-  'GET',
-  'https://random-data-api.com/api/address/random_address?size=30'
-)
-document.getElementById('xmlhttp').innerHTML = result
-xml.send()
-xml.onload = () => {
-  console.log(xml.response)
-}
+const MyRequest = new XMLHttpRequest();
+MyRequest.onload = () =>{
+  const data = JSON.parse(MyRequest.response);
+  for(let i =0; i< data.length; i++){
+    let myDiv = document.createElement("div");
+    myDiv.addEventListener("click", function(){
+      let myWindow = window.open("", "PersonalInfo");
+      myWindow.document.write( <title>User information</title>);
+      for (const property in data[i]){
+        myWindow.document.write('${propert}: ${JSON.stringify(data[i][property])} <br>');
+      }
+    });
+    myDiv.innerHTML - "<img class-'centering' src-'"+data[i].avatar + '>' +
+         "<p> Full name: " + data[i].first_name + "" + data[i].last_name+ "</p>" +
+         "<p> username: " + data[i].username+ "</p>" +
+         "<p> email: " + data[i].email + "</p>" +
+         "<p> phone number: " + data[i].phone_number +  "</p><hr>";
+      document.getElementById("usersListDiv").appendChild(myDiv);
+
+  } 
+};
+MyRequest.open('GET, "https://random-data-api.com/api/users/random_user?size=15");
+MyRequest.send();
